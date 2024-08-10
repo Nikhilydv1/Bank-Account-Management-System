04332397adf9ecfff7d98473515d629b6f076f08
Yeswanth Yadav <yeswanthyadav@Yeswanths-Air.localdomain>
2024-08-10 12:05:49 -0400

    Initial commit

---
+++ Stock.java
@@ -0,0 +1,34 @@
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Stock {
+    private String tickerSymbol;
+    private String tickerName;
+    private int marketCap;
+
+    private int sectorId;
+    private int subSectorId;
+
+    public Stock(String tickerSymbol,
+                 String tickerName,
+                 int marketCap,
+                 ) {
+        this.tickerSymbol = tickerSymbol;
+        this.tickerName = tickerName;
+        this.marketCap = marketCap;
+    }
+
+    boolean hasGoodFundamentals() {
+        boolean bool = this.currentRatio > 10;
+        return bool;
+    }
+
+    void print() {
+        System.out.println("Ticker Symbol: " + tickerSymbol);
+        System.out.println("Ticker Name: " + tickerName);
+        System.out.println("Market Cap: " + marketCap);
+        System.out.println("Sector Id: " + sectorId);
+        System.out.println("Subsector Id: " + subSectorId);
+    }
+
+}
